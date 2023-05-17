6bb13b34b0470519ea54438f5235d717a8aa3763
bao-kha <106578992+bao-kha@users.noreply.github.com>
2023-05-17 10:18:53 +0700

    bt

---
+++ FoodOrder_Kotlin/FoodOrderBasicKotlin/app/src/main/java/com/example/foodorder/constant/Constant.kt
@@ -0,0 +1,21 @@
+package com.example.foodorder.constant
+
+interface Constant {
+    companion object {
+        const val GENERIC_ERROR: String = "General error, please try again later"
+        const val PAGE_FACEBOOK: String = "https://www.facebook.com/viethoang.tranba"
+        const val LINK_FACEBOOK: String = "https://www.facebook.com/viethoang.tranba"
+        const val LINK_YOUTUBE: String = "https://www.youtube.com/channel/UCMJAF2EBZhd9cQxwq2GGxSA"
+        const val PHONE_NUMBER: String = "+0355425318"
+        const val GMAIL: String = "viethoang12b9@gmail.com"
+        const val SKYPE_ID: String = "vt_hoagn"
+        const val ZALO_LINK: String = "https://zalo.me/0355425318"
+        const val FIREBASE_URL: String = "https://foodorder-edbbf-default-rtdb.firebaseio.com"
+        const val CURRENCY: String = " 000 VNĐ"
+        const val TYPE_PAYMENT_CASH = 1
+        const val PAYMENT_METHOD_CASH: String = "Tiền mặt"
+
+        // Key Intent
+        const val KEY_INTENT_FOOD_OBJECT: String = "food_object"
+    }
+}
